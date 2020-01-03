a174045f2cef8703213f878791e77c04e6e66016
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-01-03 21:38:41 +0100

    refactor logging

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@
  * @author: blukassen
  */
 
-import { myevolux }                         from '/evolux.universe';
+import { myuniverse, myevolux }             from '/evolux.universe';
 import Repository                           from "./lib/repository.mjs";
 
 /*
@@ -28,31 +28,31 @@ export { default as Aggregate }             from './lib/aggregate.mjs';
  */
 export const service = {
     install() {
-        console.log('** tru4d install()');
+        myuniverse().logger.debug('** tru4d install()');
         myevolux().tru4d = new Repository();
     },
 
     uninstall() {
-        console.log('** tru4d uninstall()');
+        myuniverse().logger.debug('** tru4d uninstall()');
         delete myevolux().tru4d;
     },
 
     resolve() {
-        console.log('** tru4d resolve()');
+        myuniverse().logger.debug('** tru4d resolve()');
         // nothing to do
     },
 
     start() {
-        console.log('** tru4d start()');
+        myuniverse().logger.debug('** tru4d start()');
         // myevolux().tru4d;
     },
 
     stop() {
-        console.log('** tru4d stop()');
+        myuniverse().logger.debug('** tru4d stop()');
         // myevolux().tru4d;
     },
 
     update() {
-        console.log('** tru4d update()');
+        myuniverse().logger.debug('** tru4d update()');
     }
 };
