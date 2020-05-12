e2503953f28f5d72836e38bc1b2fc739866b359a
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-05-12 17:43:35 +0200

    multiple actions for command

--- index.mjs
+++ index.mjs
@@ -4,33 +4,38 @@
  * @author: blukassen
  */
 
-import { myuniverse, tservices }            from '/evolux.universe';
 import Repository                           from "./lib/repository.mjs";
 import BoundedContextBuilder                from "./lib/ctx/boundedcontextbuilder.mjs";
 
 /*
  * basic commands
  */
+/*
 export { default as Command }               from './lib/command/command.mjs';
 export { default as CreateCommand }         from './lib/command/createcommand.mjs';
 export { default as ModifyCommand }         from './lib/command/modifycommand.mjs';
 export { default as DeleteCommand }         from './lib/command/deletecommand.mjs';
-export { default as ActionCommand }         from './lib/command/actioncommand.mjs';
+*/
+// export { default as ActionCommand }         from './lib/command/actioncommand.mjs';
 
 /*
- * basi actions
+ * basic actions
  */
+/*
 export { default as Action }                from './lib/action/action.mjs';
 export { default as CreateAction }          from './lib/action/createaction.mjs';
 export { default as ModifyAction }          from './lib/action/modifyaction.mjs';
 export { default as DeleteAction }          from './lib/action/deleteaction.mjs';
+*/
 
 /*
  * bounded context
  */
+/*
 export { default as MetaBoundedContext }    from './lib/ctx/metaboundedcontext.mjs';
 export { default as BoundedContext }        from './lib/ctx/boundedcontext.mjs';
-export { default as Aggregate }             from './lib/aggregate.mjs';
+*/
+// export { default as Aggregate }             from './lib/aggregate.mjs';
 
 /*
  * context build for convenience
