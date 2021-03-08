00e3c03df8650713e3f8b74844ba442fb5a73130
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-03-08 13:35:57 +0100

    thoregon collaboration intermediate

--- libold/collection/query.mjs
+++ libold/collection/query.mjs
@@ -1,59 +1,85 @@
 /**
  * A query is a bound to a collection
  *
+ *  CAUTION: Old implementation! Don't use
  *
  * @author: Bernhard Lukassen
  */
 
 export default class Query {
 
-    constructor(collection) {
-        this.collection = collection;
+    constructor(app) {
+        this.app        = app;
+        this._listeners = [];
     }
+
+    static in(app) {
+        let query = new this(app);
+        return query;
+    }
+
+    /*
+     * setup and params
+     */
+
+    /**
+     * use the reference for the query
+     * @param ref
+     */
+    withRef(ref) {
+
+    }
+
+    /**
+     * if used within a parent e.g. in a collection
+     * @param parent
+     */
+    withParent(parent) {
+
+    }
+
     /*
      * querying
      */
 
+    /**
+     * sets a selector function which wil
+     * @param selectorfn
+     */
     select(selectorfn) {
         this.selectorfn = selectorfn;
     }
 
     /**
-     * does the selection have elements
-     */
-    has() {
-
-    }
-
-    /**
-     * get the first element
-     */
-    first() {
-
-    }
-
-    last() {
-
-    }
-
-    forEach(fn, reftype) {
-
-    }
-
-    /**
-     * works on the value items of the collection
+     * register a callback when an item is available
      * @param fn
      */
-    map(fn) {
-
+    addMutationListener(fn) {
+        this._listeners.push(fn);
     }
 
-    reduce(fn, initval) {
-
+    removeMutationListener(fn) {
+        this._listeners = this._listeners.filter(listener => listener != fn);
     }
 
-    drop() {
+    /*
+     * item handling
+     */
 
+    connect() {
+        // implement by subclass
     }
+
+    /**
+     *
+     * @param item
+     * @param id
+     * @param old
+     * @return {Promise<void>}
+     */
+    async received(item, id, old) {
+        // impelement by subclass,
+    }
+
 }
 
