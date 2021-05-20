578dbb84d87a0abc171676a07cee93e9e825d2f4
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-05-20 13:36:46 +0200

    query refactorings

--- index.mjs
+++ index.mjs
@@ -14,6 +14,10 @@ export { default as Command } from './lib/command.mjs';
 export { default as Action }  from './lib/action.mjs';
 export { default as Query }   from './lib/query.mjs';
 
+// resolver
+export { default as MatterResolver }     from './lib/matterresolver.mjs';
+export { default as QueryResolverMckup } from './lib/queryresolvermckup.mjs';
+
 // create the true4D repository and service
 export default new T4D();
 
